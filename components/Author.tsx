4add9880a6cf2234c0b2454bda6e6a70e6ec4fb4
fatihes1 <es.fatih3316@gmail.com>
2023-01-05 14:50:01 +0300

    created post detail component

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
 
-const Author = () => {
-    return (
-        <div>
-            <h1>Author</h1>
+import { grpahCMSImageLoader } from '../util';
+
+const Author = ({ author }) => (
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -top-8">
+            <Image
+                unoptimized={true}
+                loader={grpahCMSImageLoader}
+                alt={author.name}
+                height={100}
+                width={100}
+                className="align-middle rounded-full"
+                src={author.photo.url}
+            />
         </div>
-    )
-}
+        <h3 className="text-white mt-8 mb-4 text-xl font-bold">{author.name}</h3>
+        <p className="text-white text-ls">{author.bio}</p>
+    </div>
+);
 
 export default Author;
--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const CommentsForm = () => {
+const CommentsForm = ({ slug }) => {
+    const [error, setError] = useState(false);
+    const [localStorage, setLocalStorage] = useState(null);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const commentEl = useRef();
+    const nameEl = useRef();
+    const emailEl = useRef();
+    const storeDataEl = useRef();
+
     return (
-        <div>
-            <h1>CommentsForm</h1>
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comments Form</h3>
+            <div className="grid grid-cols-1 gap-4 mb-4">
+                <textarea className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+                          ref={commentEl}
+                          placeholder="Your comment here..."
+                          name="comment"
+                />
+
+
+            </div>
+            <div className="grid grid-cols-1 gap-4 mb-4"></div>
+            <div className="grid grid-cols-1 gap-4 mb-4"></div>
+            <div className="grid grid-cols-1 gap-4 mb-4"></div>
         </div>
     )
 }
